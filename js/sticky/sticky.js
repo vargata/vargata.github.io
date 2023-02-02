e603042097a6f7584dd36862b545c87b3f9f3f10
vargata <QWErty123>
2023-02-02 14:47:01 +0000

    sticky done

---
+++ js/sticky/sticky.js
@@ -0,0 +1,48 @@
+const header = document.querySelector('.main_header');
+
+header.addEventListener('mouseover', function(){
+	if(id) clearTimeout(id);
+});
+
+header.addEventListener('mouseleave', function(){	
+	if(window.scrollY > 204) id = setTimeout(hideSticky, 2000);
+});
+
+let lastScroll = 0;
+let id = null;
+
+document.addEventListener('scroll', function(){
+	var scroll = window.scrollY;
+	if(lastScroll > scroll && scroll > 204)
+		showSticky();
+	if(lastScroll < scroll && scroll > 204)
+		hideSticky();
+	if(scroll == 0)
+		removeSticky();
+		
+	lastScroll = scroll;
+	
+	if(id) clearTimeout(id);
+	if(scroll > 204) id = setTimeout(hideSticky, 2000);
+});
+
+function showSticky(){
+	if(!header.classList.contains('stickyshow')){
+		header.classList.remove('stickyhide');
+		header.classList.add('stickyshow');
+	}
+}
+
+function hideSticky(){
+	if(header.classList.contains('stickyshow')){
+		header.classList.remove('stickyshow');
+		header.classList.add('stickyhide');
+	}
+}
+
+function removeSticky(){
+	if(header.classList.contains('stickyshow') || header.classList.contains('stickyhide')){
+		header.classList.remove('stickyshow');
+		header.classList.remove('stickyhide');
+	}
+};
