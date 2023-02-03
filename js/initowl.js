4b9500f379ecd0608d98e0f136d9d5298ee4d862
vargata <QWErty123>
2023-02-03 14:18:29 +0000

    awards carousel done

--- js/initowl.js
+++ js/initowl.js
@@ -1,10 +1,18 @@
 $(document).ready(function(){
-  $(".owl-carousel").owlCarousel({
+  $(".owl").owlCarousel({
 	  loop: true,
-	  nav: true,
 	  items: 1,
 	  autoplay: true,
 	  autoplayHoverPause: true,
 	  dotsEach: true
+  });
+});
+
+$(document).ready(function(){
+  $(".owl1").owlCarousel({
+	  loop: true,
+	  items: 4,
+	  autoplay: true,
+	  dots: false
   });
 });
--- js/sticky/sticky.js
+++ js/sticky/sticky.js
@@ -1,48 +1,62 @@
+const container = document.querySelector('.sticky_container');
 const header = document.querySelector('.main_header');
 
-header.addEventListener('mouseover', function(){
-	if(id) clearTimeout(id);
-});
+let containerHeight = 0;
 
-header.addEventListener('mouseleave', function(){	
-	if(window.scrollY > 206) id = setTimeout(hideSticky, 2000);
-});
-
-let lastScroll = 0;
-let id = null;
-
-document.addEventListener('scroll', function(){
-	var scroll = window.scrollY;
-	if(lastScroll > scroll && scroll > 206)
-		showSticky();
-	if(lastScroll < scroll && scroll > 206)
-		hideSticky();
-	if(scroll == 0)
-		removeSticky();
-		
-	lastScroll = scroll;
+window.addEventListener('load', function() {
+	containerHeight = parseFloat(getComputedStyle(header).height);
+	container.style.height = containerHeight.toString() + "px";
 	
-	if(id) clearTimeout(id);
-	if(scroll > 206) id = setTimeout(hideSticky, 2000);
+	let lastScroll = 0;
+	let id = null;
+	
+	header.addEventListener('mouseover', function(){
+		if(id) clearTimeout(id);
+	});
+	
+	header.addEventListener('mouseleave', function(){	
+		if(window.scrollY > containerHeight) id = setTimeout(hideSticky, 2000);
+	});
+	
+	document.addEventListener('scroll', function(){
+		var scroll = window.scrollY;
+		if(lastScroll > scroll && scroll > containerHeight)
+			showSticky();
+		if(lastScroll < scroll && scroll > containerHeight)
+			hideSticky();
+		if(scroll == 0)
+			removeSticky();
+			
+		lastScroll = scroll;
+		
+		if(id) clearTimeout(id);
+		if(scroll > containerHeight) id = setTimeout(hideSticky, 2000);
+	});
+	
+	function showSticky(){
+		if(!header.classList.contains('stickyshow')){
+			header.classList.remove('stickyhide');
+			header.classList.add('stickyshow');
+		}
+	}
+	
+	function hideSticky(){
+		if(header.classList.contains('stickyshow')){
+			header.classList.remove('stickyshow');
+			header.classList.add('stickyhide');
+		}
+	}
+	
+	function removeSticky(){
+		if(header.classList.contains('stickyshow') || header.classList.contains('stickyhide')){
+			header.classList.remove('stickyshow');
+			header.classList.remove('stickyhide');
+		}
+	}
+
 });
 
-function showSticky(){
-	if(!header.classList.contains('stickyshow')){
-		header.classList.remove('stickyhide');
-		header.classList.add('stickyshow');
-	}
-}
-
-function hideSticky(){
-	if(header.classList.contains('stickyshow')){
-		header.classList.remove('stickyshow');
-		header.classList.add('stickyhide');
-	}
-}
-
-function removeSticky(){
-	if(header.classList.contains('stickyshow') || header.classList.contains('stickyhide')){
-		header.classList.remove('stickyshow');
-		header.classList.remove('stickyhide');
-	}
-};
+window.addEventListener('resize', function() {
+	containerHeight = parseFloat(getComputedStyle(header).height);
+	container.style.height = containerHeight.toString() + "px";
+});
