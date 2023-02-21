277e172d029ee7d5207e2572e52e033fe102d4a3
vargata <QWErty123>
2023-02-21 13:33:44 +0000

    first php push

---
+++ footer.php
@@ -0,0 +1,74 @@
+				<!-- Footer  -->
+				<footer class="main_footer">
+					<div>
+						<div class="main_footer_container">
+							<div class="top_container">
+								<ul>
+									<li><h2>ABOUT NETMATTERS</h2></li>
+									<li><a href="#">News</a></li>
+									<li><a href="#">Our Careers</a></li>
+									<li><a href="#">Our Team</a></li>
+									<li><a href="#">Privacy Policy</a></li>
+									<li><a href="#">Cookie Policy</a></li>
+									<li><a href="#">Terms &amp; Conditions</a></li>
+									<li><a href="#">Environmental Policy</a></li>
+									<li><a href="#">Contact Us</a></li>
+								</ul>
+								<ul>
+									<li><h2>OUR SERVICES</h2></li>
+									<li><a href="#">Bespoke Software</a></li>
+									<li><a href="#">IT Support</a></li>
+									<li><a href="#">Digital Marketing</a></li>
+									<li><a href="#">Telecoms Services</a></li>
+									<li><a href="#">Web Design</a></li>
+									<li><a href="#">Cyber Security</a></li>
+									<li><a href="#">Developer Training</a></li>
+								</ul>
+							</div>
+							<div class="bottom_container">
+								<ul>
+									<li><h2>CAMBRIDGE OFFICE</h2></li>
+									<li>Unit 1.31,</li>
+									<li>St John's Innovation Centre,</li>
+									<li>Cowley Road, Milton,</li>
+									<li>Cambridge,</li>
+									<li>CB4 0WS</li>
+									<li>&nbsp;</li>
+									<li>Tel: <a href="#">01223 37 57 72</a></li>
+								</ul>
+								<ul>
+									<li><h2>WYMONDHAM OFFICE</h2></li>
+									<li>Unit 15,</li>
+									<li>Penfold Drive,</li>
+									<li>Gateway 11 Business Park,</li>
+									<li>Wymondham, Norfolk,</li>
+									<li>NR18 0WZ</li>
+									<li>&nbsp;</li>
+									<li>Tel: <a href="#">01603 70 40 20</a></li>
+								</ul>
+								<ul>
+									<li><h2>GREAT YARMOUTH OFFICE</h2></li>
+									<li>Suite F23,</li>
+									<li>Beacon Innovation Centre,</li>
+									<li>Beacon Park, Gorleston,</li>
+									<li>Great Yarmouth, Norfolk,</li>
+									<li>NR31 7RA</li>
+			 						<li>&nbsp;</li>
+									<li>Tel: <a href="#">01493 60 32 04</a></li>
+								</ul>
+							</div>
+						</div>
+						<div class="copy_container">
+							<div class="copyright">
+								&copy; Copyright Netmatters 2022.&nbsp;<br class="important"/>All rights reserved.<span>&nbsp;-&nbsp;<a href="#">Sitemap</a></span>
+							</div>
+							<div class="socialmedia">
+								<a href="#" class="sm_icon1"><i class="sm_icon fa-brands fa-facebook-f"></i></a>
+								<a href="#" class="sm_icon2"><i class="sm_icon fa-brands fa-twitter"></i></a>
+								<a href="#" class="sm_icon3"><i class="sm_icon fa-brands fa-instagram"></i></a>
+								<a href="#" class="sm_icon4"><i class="sm_icon fa-brands fa-linkedin-in"></i></a>
+							</div>
+						</div>
+					</div>
+				</footer>
+				<!-- end of Footer  -->
