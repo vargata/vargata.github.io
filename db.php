eea18562131f961da153dcb6680085058daac411
vargata <QWErty123>
2023-02-23 16:05:24 +0000

    adding database control

---
+++ db.php
@@ -0,0 +1,67 @@
+<?php
+
+class db
+{    
+    private $conn;
+    
+    const db_host = "localhost";
+    const db_name = "db_netmatters";
+    const db_user = "contact_user";
+    const db_pwd = "contact_pwd";
+    
+    private $sql_list = array();
+    private $param_list = array();
+    
+    function add_query($sql_name, $sql){
+        $this->sql_list[$sql_name] = $sql;
+    }
+    
+    function add_params($sql_name, array $params){
+        $this->param_list[$sql_name] = $params;
+    }
+    
+    function connect_db(){
+        mysqli_report(MYSQLI_REPORT_OFF);
+        $this->conn = @mysqli_connect(self::db_host, self::db_user, self::db_pwd, self::db_name);
+        if(!$this->conn){
+            echo "<label class='msglabel'>"."Connection error\nPlease try again later"."</label>".PHP_EOL;
+        }
+    }
+    
+    function get_Data($sql_name){
+        if($this->conn){
+            $returndata = array();
+            
+            $sql = $this->sql_list[$sql_name];
+            $stmt = mysqli_prepare($this->conn, $sql);
+            $stmt->bind_param($paramstring, $this->param_list[$sql_name]);
+            $stmt->execute();
+            
+            while($row = mysqli_fetch_all($result))
+            {
+                $returndata[] = $row;
+            }
+            return $returndata;
+        }
+    }
+    
+    Function save_Data($fname, $sname, $email, $phone, $subject, $message){
+        $returndata = false;
+        if($this->conn){
+            $sql_savedata = "CALL add_Message(?, ?, ?, ?, ?, ?)";
+            //$returndata = $this->conn->query($sql_savedata);
+            $stmt = mysqli_prepare($this->conn, $sql_savedata);
+            $stmt->bind_param('ssssis', $fname, $sname, $email, $phone, $subject, $message);
+            $stmt->execute();
+            $returndata = $stmt->affected_rows;
+        }
+        return $returndata;
+    }
+    
+    function close_db(){
+        if($this->conn)
+            mysqli_close($this->conn);
+    }
+}
+
+?>
